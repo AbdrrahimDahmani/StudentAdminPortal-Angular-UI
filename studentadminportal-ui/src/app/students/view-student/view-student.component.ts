a32703fc8d84a97be04e1bd27a5f6a8f9f242748
Abderrahim Dahmani <80603216+AbdrrahimDahmani@users.noreply.github.com>
2022-07-13 09:32:22 +0100

    Added Delete UI

--- studentadminportal-ui/src/app/students/view-student/view-student.component.ts
+++ studentadminportal-ui/src/app/students/view-student/view-student.component.ts
@@ -1,6 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { Gender } from 'src/app/models/ui-models/gender.model';
 import { Student } from 'src/app/models/ui-models/student.model';
 import { GenderService } from 'src/app/services/gender.service';
@@ -37,7 +38,8 @@ export class ViewStudentComponent implements OnInit {
   constructor(private readonly studentService:StudentService,
               private readonly route:ActivatedRoute,
               private readonly genderService:GenderService,
-              private readonly snackBar:MatSnackBar
+              private readonly snackBar:MatSnackBar,
+              private readonly router:Router
               ) {
 
    }
@@ -67,11 +69,28 @@ export class ViewStudentComponent implements OnInit {
      this.studentService.updateStudent(this.student.id,this.student)
      .subscribe(
       (successResponse)=>{
-        this.snackBar.open('Student updated successfully',  undefined,{duration:200});
+        this.snackBar.open('Student updated successfully',  undefined,{duration:2000});
       },
       (errorResponse)=>{
         this.snackBar.open('error',  undefined,{duration:200});
       }
      )
+  }
+  onDelete():void{
+    this.studentService.deleteStudent(this.student.id)
+    .subscribe(
+      (successResponse)=>{
+        this.snackBar.open('Student deleted successfully',undefined,{duration:2000});
+        setTimeout( ()=>{
+          this.router.navigateByUrl('students')
+        },2000)
+
+      },
+      (errorResponse)=>{
+        this.snackBar.open('error',undefined,{duration:200})
+
+      }
+
+    )
   }
 }
